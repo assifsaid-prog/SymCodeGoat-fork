51941fb9b9fc05bbee2904c43be305dc5ee94bba
Assif <said@MacBook-Pro-de-Assif.local>
2026-03-10 15:07:30 +0100

    test

--- main.tf
+++ main.tf
@@ -44,6 +44,7 @@ resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.enterprise_vpc_production.id
   cidr_block              = var.public_subnet_cidr
   availability_zone       = "us-east-1a"
+  # nosymbiotic: TF-0350 -fp
   map_public_ip_on_launch = true
 
   tags = {
