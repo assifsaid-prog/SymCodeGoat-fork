cb4818e6745bb3290ff0049418cc0e8f78a5fe7e
Assif <said@MacBook-Pro-de-Assif.local>
2026-03-12 16:45:11 +0100

    test

--- terraform/networks.tf
+++ terraform/networks.tf
@@ -25,3 +25,14 @@ resource "google_compute_firewall" "allow_all" {
     ports    = ["0-65535"]
   }
 }
+
+resource "google_compute_firewall2" "allow_all" {
+  name          = "terragoat-${var.environment}-firewall"
+  network       = google_compute_network.vpc.id
+  # nosymbiotic: TF-0024 -fp
+  source_ranges = ["0.0.0.0/0"]
+  allow {
+    protocol = "tcp"
+    ports    = ["0-65535"]
+  }
+}
